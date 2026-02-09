d46d8d45dd5701a99a0a66e5bc21810fed0a6b37
yuyingcr25-discover <yuyingcr25@gmail.com>
2026-02-09 10:13:26 -0800

    yuying update

--- src/components/assistant/index.ts
+++
@@ -1,3 +0,0 @@
-export { FloatingAssistant } from './FloatingAssistant';
-export { InlineSuggestion, useInlineSuggestion } from './InlineSuggestion';
-export { SuggestionToasts } from './SuggestionToast';
--- src/pages/PrototypeWorkflow.tsx
+++
@@ -1,9 +0,0 @@
-import PrototypeSimulator from '../components/prototype/PrototypeSimulator';
-
-export default function PrototypeWorkflow() {
-  return (
-    <div>
-      <PrototypeSimulator />
-    </div>
-  );
-}
